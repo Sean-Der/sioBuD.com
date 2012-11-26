e7480d4c79e7b7c142478c6f0c51affde464ceb6
Sean DuBois <sean@siobud.com>
2012-11-26 18:06:01 -0500

    Bad indenting habits die hard

--- blogWithDB.scm
+++ blogWithDB.scm
@@ -4,31 +4,23 @@
 		`(div (@ (class "article"))
 		 	(h2 ,(car n))
 			(p ,(car (cdr n)))
-			(p (a (@ (href "blog/",(car (cdr (cdr n))))) "Read More"))
-		)
-	) ($db "SELECT title,summary,url from blogPosts ORDER BY id DESC LIMIT 5"))
-)
+			(p (a (@ (href "blog/",(car (cdr (cdr n))))) "Read More")))) 
+		($db "SELECT title,summary,url from blogPosts ORDER BY id DESC LIMIT 5")))
 
 (define (getBlogPost blogURL) 
 	(if (= 1 (car (car ($db "SELECT COUNT(url) from blogPosts where url = $1" values: (list blogURL)))))
 		`( ,(car (car ($db "SELECT COUNT(url) from blogPosts where url = $1" values: (list blogURL)))))
-		`(h2 "Sorry! I was not able to find the blog post you are looking for")
-	)
-)
+		`(h2 "Sorry! I was not able to find the blog post you are looking for")))
 
 ;Main page for all blog posts
 (define-page "/blog"
   (lambda ()
 		(set-page-title! "(sioBuD.com (Blog))")
-			(template `(div ,(blogPostOverview)))
-	)
-)
+			(template `(div ,(blogPostOverview)))))
 
 ;Page to fetch a specific blog post
 (define-page (regexp "/blog/.*")
 	(lambda (path)
 		(define blogPost (string-substitute "/blog/" "" path))
 		(set-page-title! "sioBuD.com")
-		(template `(div (@ (class "article")) ,(getBlogPost blogPost)))
-	)
-)
+		(template `(div (@ (class "article")) ,(getBlogPost blogPost)))))
