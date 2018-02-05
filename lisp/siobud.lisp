dc674458eaf434c5289e3b4126da8b572bc17797
Sean DuBois <sean@siobud.com>
2018-02-05 02:49:08 +0000

    Bump resume

--- lisp/siobud.lisp
+++ lisp/siobud.lisp
@@ -1,6 +1,6 @@
 (in-package #:siobud)
 
-(defvar *urls* (list "/" 'main "/interests/computing" 'computing "/interests/music" 'music "/blog" 'blog))
+(defvar *urls* (list "/" 'main "/interests/computing" 'computing "/blog" 'blog))
 
 (defun start-server ()
   (swank:create-server :style :spawn :dont-close t)
@@ -14,7 +14,7 @@
             ((:h1 :style "font-size: 50px") "Hi!")
             ((:h1 :style "font-size: 30px") "My name is " ((:span :style "font-weight: 700") "Sean DuBois") " and this is my webpage")
             (:br)
-            ((:h1 :style "font-size: 30px") "My day job involves " ((:a :class "fancyAnchor" :href "/interests/computing" :style "font-weight: 700") "Programming") ", but I also love collecting " ((:a :class "fancyAnchor" :href "/interests/music" :style "font-weight: 700") "Vinyl") ". Take a look around! Make sure to check out my  "((:a :class "fancyAnchor" :href "/blog" :style "font-weight: 700") "Blog") " and  "  ((:a :class "fancyAnchor" :href "http://github.com/sean-der" :style "font-weight: 700") "GitHub") " to see what I am working on.")
+            ((:h1 :style "font-size: 30px") "My day job involves " ((:a :class "fancyAnchor" :href "/interests/computing" :style "font-weight: 700") "Programming") "! Make sure to check out my  "((:a :class "fancyAnchor" :href "/blog" :style "font-weight: 700") "Blog") " and  "  ((:a :class "fancyAnchor" :href "http://github.com/sean-der" :style "font-weight: 700") "GitHub") " to see what I am working on.")
             ((:h1 :style "font-size: 30px; padding-top: 4em") "Important Note!")
             (:p "I take your privacy very seriously. When you access this website I don't fetch anything from an external " ((:a :class "fancyAnchor" :href "https://en.wikipedia.org/wiki/Content_delivery_network") "CDN") )
             (:p "I do not employ any sort of "((:a :class "fancyAnchor" :href "https://en.wikipedia.org/wiki/Google_Analytics") "Analytics") " I am not interested in tracking you.")
